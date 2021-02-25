2d4c3bbfc0d80ab54cb0981f02ac68408e5237a3
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-25 13:32:21 -0800

    implementation to return a list of all cars stored in-memory

--- src/main/java/com/carmaxx/rental/dao/CarDataService.java
+++ src/main/java/com/carmaxx/rental/dao/CarDataService.java
@@ -6,6 +6,9 @@ import java.util.UUID;
 
 import com.carmaxx.rental.model.Car;
 
+import org.springframework.stereotype.Repository;
+
+@Repository("in-memory data")
 public class CarDataService implements CarDAO {
 
     private static List<Car> DB = new ArrayList<>();
@@ -14,5 +17,10 @@ public class CarDataService implements CarDAO {
     public int insertCar(UUID id, Car car) {
         DB.add(new Car(id, car.getName()));
         return 1;
+    }
+
+    @Override
+    public List<Car> selectAllCars() {
+        return DB;
     }
 }
