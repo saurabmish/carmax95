41f4bf6aac7bfa8c75b49a43ce37e00533a68881
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-25 13:29:09 -0800

    endpoint definition and request handling

--- src/main/java/com/carmaxx/rental/api/CarController.java
+++ src/main/java/com/carmaxx/rental/api/CarController.java
@@ -1,17 +1,35 @@
 package com.carmaxx.rental.api;
 
+import java.util.List;
+
 import com.carmaxx.rental.model.Car;
 import com.carmaxx.rental.service.CarService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/car")
+@RestController
 public class CarController {
     
     private final CarService carService;
 
+    @Autowired
     public CarController(CarService carService) {
         this.carService = carService;
     }
 
-    public void addCar(Car car) {
+    @PostMapping
+    public void addCar(@RequestBody Car car) {
         carService.addCar(car);
+    }
+
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
     }
 }
