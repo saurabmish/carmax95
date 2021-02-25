3851e54e27772c4bfd7646a670756ce94295aaff
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-25 13:32:47 -0800

    object to JSON return type definition

--- src/main/java/com/carmaxx/rental/model/Car.java
+++ src/main/java/com/carmaxx/rental/model/Car.java
@@ -2,11 +2,14 @@ package com.carmaxx.rental.model;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Car {
     private final UUID id;
     private final String name;
 
-    public Car(UUID id, String name) {
+    public Car(@JsonProperty("id") UUID id, 
+               @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
