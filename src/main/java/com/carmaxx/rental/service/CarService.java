f4930ed4a014b41a311744bac70c6d4766fa511a
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-25 13:33:28 -0800

    calls car interface to get all cars

--- src/main/java/com/carmaxx/rental/service/CarService.java
+++ src/main/java/com/carmaxx/rental/service/CarService.java
@@ -1,17 +1,29 @@
 package com.carmaxx.rental.service;
 
+import java.util.List;
+
 import com.carmaxx.rental.dao.CarDAO;
 import com.carmaxx.rental.model.Car;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CarService {
     
     private final CarDAO carDAO;
 
-    public CarService(CarDAO carDAO) {
+    @Autowired
+    public CarService(@Qualifier("in-memory data") CarDAO carDAO) {
         this.carDAO = carDAO;
     }
 
     public int addCar(Car car) {
         return carDAO.insertCar(car);
+    }
+
+    public List<Car> getAllCars() {
+        return carDAO.selectAllCars();
     }
 }
