38712c0b07db06e34f172cd9c38e3bdbc98862b1
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-26 17:27:13 -0800

    added endpoints for update and delete operations

--- src/main/java/com/carmaxx/rental/api/CarController.java
+++ src/main/java/com/carmaxx/rental/api/CarController.java
@@ -1,13 +1,17 @@
 package com.carmaxx.rental.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.carmaxx.rental.model.Car;
 import com.carmaxx.rental.service.CarService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +35,21 @@ public class CarController {
     @GetMapping
     public List<Car> getAllCars() {
         return carService.getAllCars();
+    }
+
+    @GetMapping(path = "{id}")
+    public Car getCar(@PathVariable("id") UUID id) {
+        return carService.getCar(id)
+                .orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteCar(@PathVariable("id") UUID id) {
+        carService.deleteCar(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateCar(@PathVariable("id") UUID id, @RequestBody Car carToUpdate) {
+        carService.updateCar(id, carToUpdate);
     }
 }
