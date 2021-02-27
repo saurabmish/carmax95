58e58c5d146c66516bc166701b8fd22f5d265546
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-26 17:30:21 -0800

    method definitions for update and delete functionality

--- src/main/java/com/carmaxx/rental/dao/CarDAO.java
+++ src/main/java/com/carmaxx/rental/dao/CarDAO.java
@@ -3,16 +3,23 @@ package com.carmaxx.rental.dao;
 import com.carmaxx.rental.model.Car;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CarDAO {
 
-    int insertCar(UUID id, Car car);
+    List<Car> selectAllCars();
 
+    Optional<Car> selectCar(UUID id);
+
+    int deleteCar(UUID id);
+
+    int updateCar(UUID id, Car car);
+
+    int insertCar(UUID id, Car car);
+    
     default int insertCar(Car car) {
         UUID id = UUID.randomUUID();
         return insertCar(id, car);
-    }
-
-    List<Car> selectAllCars();
+    }    
 }
