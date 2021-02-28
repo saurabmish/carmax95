d5f00c75006ccfd0a3ce42419b8ed8aa91c7d15b
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-28 15:15:18 -0800

    calls DAO for CRUD operations

--- src/main/java/com/carmaxx/rental/service/CarService.java
+++ src/main/java/com/carmaxx/rental/service/CarService.java
@@ -1,15 +1,14 @@
 package com.carmaxx.rental.service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
 import com.carmaxx.rental.dao.CarDAO;
 import com.carmaxx.rental.model.Car;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CarService {
