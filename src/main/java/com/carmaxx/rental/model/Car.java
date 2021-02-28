4942b3463058c9a214367244f2e2480b0b20429f
Saurabh Mishra <saurab.mish@gmail.com>
2021-02-28 15:09:58 -0800

    added additional attributes to data model

--- src/main/java/com/carmaxx/rental/model/Car.java
+++ src/main/java/com/carmaxx/rental/model/Car.java
@@ -1,24 +1,46 @@
 package com.carmaxx.rental.model;
 
-import java.util.UUID;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Car {
-    private final UUID id;
-    private final String name;
+import java.util.UUID;
 
-    public Car(@JsonProperty("id") UUID id, 
-               @JsonProperty("name") String name) {
+public class Car {
+
+    private final UUID id;
+    private final Character available;
+    private final String name;
+    private final String manufacturer;
+    private final Integer numPreviousOwners;
+
+    public Car(@JsonProperty("id") UUID id,
+               @JsonProperty("available") Character available,
+               @JsonProperty("name") String name,
+               @JsonProperty("manufacturer") String manufacturer,
+               @JsonProperty("owned times") Integer numPreviousOwners) {
         this.id = id;
+        this.available = available;
         this.name = name;
+        this.manufacturer = manufacturer;
+        this.numPreviousOwners = numPreviousOwners;
     }
 
     public UUID getId() {
         return id;
     }
 
+    public Character getAvailable() {
+        return available;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public Integer getNumPreviousOwners() {
+        return numPreviousOwners;
     }
 }
